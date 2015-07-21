5e7d16e0a5d4079f5c51e7bf07a1f22923ded904
Idris Mokhtarzada <idris.m@gmail.com>
2015-07-21 14:08:09 -0400

    post, not get

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/cloudmailin', function(req, res, next) {
+router.post('/cloudmailin', function(req, res, next) {
   var subject = req.body.headers.Subject;
   var body = req.body.html;
 
