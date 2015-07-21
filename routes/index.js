f219ac8fcc9cd4b824c312fd81b4936eabfbe9ed
Idris Mokhtarzada <idris.m@gmail.com>
2015-07-21 14:05:13 -0400

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var sonarEmailParser = require('../lib/sonarEmailParser');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/cloudmailin', function(req, res, next) {
+  var subject = req.body.headers.Subject;
+  var body = req.body.html;
+
+  var msg = sonarEmailParser.parse(subject, body);
+
+  console.log(msg.subject, msg.body);
+});
+
+module.exports = router;
